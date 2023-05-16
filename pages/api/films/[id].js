688d5c1b87d899491a2c561d08b54c89b8a04f71
RogelioLB <rogelio20052011@gmail.com>
2023-05-15 18:54:58 -0700

    Bug Fix English

--- pages/api/films/[id].js
+++ pages/api/films/[id].js
@@ -1,6 +1,6 @@
 import data from "../../../data.json"
 export default function handler(req, res) {
-    const { id,lang } = req.query
+    const { id, lang="en" } = req.query
     if(!id) res.send({error:true,message:"No id provided"})
     const {films} = data[lang]
     const film = films.find((film)=>film.id === id)
